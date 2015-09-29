0ae249537631e669f00dbdbbad31086711b3c621
Jose Olcese <jolcese@gmail.com>
2015-09-29 10:42:20 -0700

    Update cli_tools.rb

--- definitions/cli_tools.rb
+++ definitions/cli_tools.rb
@@ -8,24 +8,24 @@ define :cli_tools, :extension => '.zip' do
     use_conditional_get !node['aws_developer_tools']['force_download?']
     use_etag true
     use_last_modified false
-    notifies :run, 'execute[cleanup old installs]'
+    notifies :run, 'execute[cleanup old install ' + params[:name] + ']', :immediately
   end
-  
-  execute 'cleanup old installs' do
+
+  execute "cleanup old install #{params[:name]}" do
     cwd '/tmp'
     command "rm -rf #{params[:name]} && mkdir #{params[:name]}"
     action 'nothing'
-    notifies :run, 'execute[extract the aws tool]'
+    notifies :run, 'execute[extract the aws tool ' + params[:name] + ']', :immediately
   end
-  
-  execute 'extract the aws tool' do
+
+  execute "extract the aws tool #{params[:name]}" do
     cwd "/tmp/#{params[:name]}"
     command "unzip -o ../#{params[:name] + params[:extension]}"
     action 'nothing'
-    notifies :run, 'ruby_block[copy the tools to the target directory]'
+    notifies :run, 'ruby_block[copy the tools to the target directory ' + params[:name] + ']', :immediately
   end
 
-  ruby_block 'copy the tools to the target directory' do
+  ruby_block "copy the tools to the target directory #{params[:name]}" do
     block do
       FileUtils.cd("/tmp/#{params[:name]}") do
         source = Dir['*'].detect { |file| File.directory? file }
@@ -45,30 +45,42 @@ define :cli_tools, :extension => '.zip' do
       end
     end
     action 'nothing'
-    notifies :create, 'template[/etc/profile.d/ec2_tools.sh]'
-    notifies :create, 'template[' + node[:aws_developer_tools][:aws_tools_credentials][:location] + ']'
-    notifies :create, 'template[/etc/profile.d/' + params[:name] + '.sh]'
-    notifies :create, 'template[/etc/profile.d/aws_tools.sh]'
+    notifies :create, 'template[ec2_tools ' + params[:name] + ']', :immediately
+    notifies :create, 'template[location ' + params[:name] + ']', :immediately
+    notifies :create, 'template[' + params[:name] + '.sh]', :immediately
+    notifies :create, 'template[aws_tools.sh ' + params[:name] + ']', :immediately
   end
 
-  template '/etc/profile.d/ec2_tools.sh' do
+  template "ec2_tools #{params[:name]}" do
+    source "ec2_tools.sh.erb"
+    path "/etc/profile.d/ec2_tools.sh"
     mode 0755
     only_if { AwsDeveloperTools.type?(params[:name]) == :ec2 }
+    action 'nothing'
   end
 
-  template "#{node['aws_developer_tools']['aws_tools_credentials']['location']}" do
+  template "location #{params[:name]}" do
+    source "credentials.erb"
+    path "#{node['aws_developer_tools']['aws_tools_credentials']['location']}"
     mode node['aws_developer_tools']['aws_tools_credentials']['permission']
     not_if { AwsDeveloperTools.type?(params[:name]) == :ec2 }
+    action 'nothing'
   end
 
-  template "/etc/profile.d/#{params[:name]}.sh" do
+  template "#{params[:name]}.sh" do
+    source "#{params[:name]}.sh.erb"
+    path "/etc/profile.d/#{params[:name]}.sh"
     mode 0755
     not_if { AwsDeveloperTools.type?(params[:name]) == :ec2 }
+    action 'nothing'
   end
 
-  template '/etc/profile.d/aws_tools.sh' do
+  template "aws_tools.sh #{params[:name]}" do
+    source "aws_tools.sh.erb"
+    path "/etc/profile.d/aws_tools.sh #{params[:name]}"
     mode 0755
     not_if { AwsDeveloperTools.type?(params[:name]) == :ec2 }
+    action 'nothing'
   end
 
 end
